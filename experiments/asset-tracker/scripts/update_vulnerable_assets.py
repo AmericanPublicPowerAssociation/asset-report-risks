3db061ed5d5a6cb21d257cbfba7124b0551bbd29
Roy Hyunjin Han <rhh@crosscompute.com>
2019-04-19 11:41:07 -0400

    Remove tex file

--- experiments/asset-tracker/scripts/update_vulnerable_assets.py
+++ experiments/asset-tracker/scripts/update_vulnerable_assets.py
@@ -1,13 +1,3 @@
-import argparse
-import sys
-
-from pymongo import MongoClient
-from pyramid.paster import bootstrap, setup_logging
-from sqlalchemy.exc import OperationalError
-
-from asset_tracker.models import Asset, VulnerableAsset
-
-
 def get_vulnerabilities(asset, mongo_cursor):
     vulnerabilities = query_nvd(mongo_cursor, asset.name)
     return [{
@@ -20,24 +10,6 @@ def get_vulnerabilities(asset, mongo_cursor):
 
 def query_nvd(cursor, product_name=None, vendor_name=None, version_value=None):
     product_query, vendor_query, version_query = {}, {}, {}
-    if not (product_name or vendor_name or version_value):
-        return None
-    if product_name:
-        product_query_string = \
-            'cve.affects.vendor.vendor_data.product.product_data.product_name'
-        product_query = {
-            product_query_string: product_name}
-    if vendor_name:
-        vendor_query_string = \
-            'cve.affects.vendor.vendor_data.vendor_name'
-        vendor_query = {
-            vendor_query_string: vendor_name}
-    if version_value:
-        version_query_string = \
-            'cve.affects.vendor.vendor_data.product.product_data.version.' + \
-            'version_data.version_value'
-        version_query = {
-            version_query_string: version_value}
     results = []
     for x in cursor.find(
             {'$and': [product_query, vendor_query, version_query]}):
@@ -76,55 +48,3 @@ def search_vulnerabilities(dbsession, mongo_cursor):
                 score=vulnerability['score'])
             dbsession.add(va)
             dbsession.commit()
-
-
-def parse_args(argv):
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        'config_uri',
-        help='Configuration file, e.g., development.ini',
-    )
-    return parser.parse_args(argv[1:])
-
-
-def get_mongo_connection():
-    URL = 'mongodb://localhost:27017/Vulnerabilities'
-    MONGO_DICT = dict(
-        database='Vulnerabilities',
-        collection='NVD',
-        document='CVE_Items')
-    client = MongoClient(URL)
-    database = client[MONGO_DICT['database']]
-    collection = database[MONGO_DICT['collection']]
-    return collection[MONGO_DICT['document']]
-
-
-def main(argv=sys.argv):
-    args = parse_args(argv)
-    setup_logging(args.config_uri)
-    env = bootstrap(args.config_uri)
-    mongo_cursor = get_mongo_connection()
-
-    try:
-        with env['request'].tm:
-            dbsession = env['request'].dbsession
-            search_vulnerabilities(dbsession, mongo_cursor)
-    except OperationalError:
-        print('''
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to initialize your database tables with `alembic`.
-    Check your README.txt for description and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-            ''')
-
-
-if __name__ == '__main__':
-    cursor = get_mongo_connection()
-    vulnerabilities = query_nvd(cursor, 'mds_pulsenet')
-    # import pprint; print = pprint.PrettyPrinter(indent=4).pprint
-    print(vulnerabilities)
