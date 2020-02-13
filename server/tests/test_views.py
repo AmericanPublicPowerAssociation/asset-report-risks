a9a42e2198f0037e6936c6921e3994058eac83b1
Roy Hyunjin Han <rhh@crosscompute.com>
2020-02-13 16:54:26 +0000

    Add test for get_risks_json

--- server/tests/test_views.py
+++ server/tests/test_views.py
@@ -2,6 +2,26 @@ from asset_report_risks.views import get_risks_json
 
 
 class TestGetRisksJson(object):
-    
-    def test_accept_parameters(self, website_request, db):
-        pass
+
+    def test_accept_parameters(self, website_request, db, mocker):
+        mocker.patch('asset_report_risks.views.get_risks', return_value=[
+            {
+                'assetId': 'X',
+                'meterIds': [1, 2, 3],
+                'vulnerabilityUri': 'X',
+                'threatScore': 100,
+                'threatDescription': 'X',
+                'vulnerabilityUrl': 'X',
+                'vulnerabilityDate': '20200101',
+            },
+        ])
+        website_response_ds = get_risks_json(website_request)
+        assert len(website_response_ds) == 1
+        d = website_response_ds[0]
+        assert d['assetId'] == 'X'
+        assert d['meterIds'] == [1, 2, 3]
+        assert d['vulnerabilityUri'] == 'X'
+        assert d['threatScore'] == 100
+        assert d['threatDescription'] == 'X'
+        assert d['vulnerabilityUrl'] == 'X'
+        assert d['vulnerabilityDate'] == '20200101'
