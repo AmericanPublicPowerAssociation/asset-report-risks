bb8297a8033cb22835ba018915b3413c213005bb
Roy Hyunjin Han <rhh@crosscompute.com>
2020-02-15 09:59:53 -0500

    Use load_json https://github.com/AmericanPublicPowerAssociation/asset-tracker/issues/42

--- server/asset_report_risks/constants/assets.py
+++ server/asset_report_risks/constants/assets.py
@@ -1,6 +1,6 @@
-import json
 from os.path import join
 
+from asset_tracker.macros import load_json
 from asset_tracker.routines.assets import absorb_asset_types
 
 from . import CONSTANTS_FOLDER
@@ -10,5 +10,4 @@ def includeme(config):
     absorb_asset_types(ASSET_TYPES)
 
 
-ASSET_TYPES_PATH = join(CONSTANTS_FOLDER, 'assetTypes.json')
-ASSET_TYPES = json.load(open(ASSET_TYPES_PATH, 'rt'))
+ASSET_TYPES = load_json(join(CONSTANTS_FOLDER, 'assetTypes.json'))
