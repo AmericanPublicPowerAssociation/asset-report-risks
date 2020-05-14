fe9b87962ec433aea7f21d99dda1c60ee3e190f1
Roy Hyunjin Han <rhh@crosscompute.com>
2020-05-14 08:57:39 -0400

    Fix webpack configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,9 +2,11 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+  /*
   optimization: {
     minimize: false,
   },
+  */
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
