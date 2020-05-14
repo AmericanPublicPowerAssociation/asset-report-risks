f02746a3f072818b67a6f3f5088e41c5c2f8a63b
Roy Hyunjin Han <rhh@crosscompute.com>
2020-05-14 06:59:10 -0400

    Attempt using parcel

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,25 +1,35 @@
 module.exports = {
   env: {
+    browser: true,
     es6: true,
   },
   extends: [
+    'plugin:react/recommended',
     'standard',
-    'standard-react'
   ],
-  plugins: [
-    'react'
-  ],
-  parserOptions: {
-    'sourceType': 'module'
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
     'object-curly-spacing': ['error', 'always'],
     'quotes': ['error', 'single'],
+    'quote-props': ['error', 'consistent'],
     'semi': ['error', 'never'],
     'space-before-function-paren': ['error', {
       'anonymous': 'always',
-      'named': 'never'
-    }]
-  }
+      'named': 'never',
+    }],
+  },
 }
--- client/README.md
+++ client/README.md
@@ -1,6 +1,33 @@
 # Asset Report Risks Client
 
-    cd ~/Projects/
+## Installation
+
     git clone git@github.com:AmericanPublicPowerAssociation/asset-report-risks
-    cd ~/Projects/asset-report-risks/client
-    npm install
+    cd asset-report-risks/client
+    yarn install
+
+## Configuration from Scratch
+
+Follow these steps to recreate the package configuration from scratch.
+
+```
+yarn init -y
+yarn add --dev \
+    @babel/core \
+    @babel/preset-env \
+    @babel/preset-react \
+    eslint eslint-plugin-react \
+    parcel-bundler
+yarn add --peer \
+    @material-ui/core \
+    @material-ui/icons \
+    downshift \
+    material-table \
+    react \
+    react-dom \
+    react-redux \
+    react-router-dom \
+    redux-saga \
+    reselect
+eslint --init
+```
